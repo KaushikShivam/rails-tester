dd8f0ad2cb2c47daf34ad97e5e1a1c94939119c6
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-27 03:58:52 +0530

    Fix stickler errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def index
     @name = 'This is an index action'
   end
-  
-  def show 
+
+  def show
     @name = 'This is a show action'
   end
-  
-  def new 
+
+  def new
     @name = 'This is a new action'
   end
-  
+
   def edit
     @name = 'this is an edit action'
   end
-  
+
   def create
     @name = 'This is a post route'
   end
-  
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 
 url = 'http://localhost:3000/users'
