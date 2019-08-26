dabc9cb789a8c0440a2977287438f6d170b0605c
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-27 03:57:02 +0530

    Finish test routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    @name = 'This is an index action'
+  end
+  
+  def show 
+    @name = 'This is a show action'
+  end
+  
+  def new 
+    @name = 'This is a new action'
+  end
+  
+  def edit
+    @name = 'this is an edit action'
+  end
+  
+  def create
+    @name = 'This is a post route'
+  end
+  
+end
